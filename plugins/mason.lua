246aab0ffe7d5ad73f495584d5195c76c80359e9
ForAeons <w.xy020203@gmail.com>
2023-09-24 21:12:53 +0800

    add rust analyser and fix indentation

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -3,22 +3,23 @@ return {
     "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {
-      -- LSPs
-      "bashls",
-      "clangd",
-      "cssls",
-      "dockerls",
-      "eslint",
-      "gopls",
-      "golangci_lint_ls",
-      "html",
-      "jsonls",
-      "jdtls",
-      "tsserver",
-      "lua_ls",
-      "sqlls",
-      "svelte",
-      "tailwindcss",
+        -- LSPs
+        "bashls",
+        "clangd",
+        "cssls",
+        "dockerls",
+        "eslint",
+        "gopls",
+        "golangci_lint_ls",
+        "html",
+        "jsonls",
+        "jdtls",
+        "tsserver",
+        "lua_ls",
+        "sqlls",
+        "svelte",
+        "tailwindcss",
+        "rust_analyzer",
       }
     }
   },
@@ -26,14 +27,14 @@ return {
     "jay-babu/mason-null-ls.nvim",
     opts = {
       ensure_installed = {
-      -- Formatters
-      "prettier",
-      "gofumpt",
-      "goimports",
-      "goimports-reviser",
-      -- Linters
-      "eslint_d",
-      "golangci-lint",
+        -- Formatters
+        "prettier",
+        "gofumpt",
+        "goimports",
+        "goimports-reviser",
+        -- Linters
+        "eslint_d",
+        "golangci-lint",
       }
     }
   },
