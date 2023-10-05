e075445297df5255c3f8d6bd1c26dc2a0f73c153
ForAeons <w.xy020203@gmail.com>
2023-10-05 09:19:29 +0800

    edit ts config

--- plugins/languages/typescript.lua
+++ plugins/languages/typescript.lua
@@ -1,33 +1,27 @@
+local utils = require "astronvim.utils"
+
 return {
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.typescript-deno" },
+  { import = "astrocommunity.pack.json" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "javascript", "typescript", "tsx" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "denols") end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "js") end,
+  },
   {
     "sigmasd/deno-nvim",
-    -- HACK: This disables tsserver if denols is attached.
-    -- A solution that only enables the required lsp should replace it.
-    opts = function(_, opts)
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local bufnr = args.buf
-          local curr_client = vim.lsp.get_client_by_id(args.data.client_id)
-          -- if deno attached, stop all typescript servers
-          if curr_client.name == "denols" then
-            vim.lsp.for_each_buffer_client(bufnr, function(client, client_id)
-              if client.name == "tsserver" then vim.lsp.stop_client(client_id, true) end
-            end)
-            -- if tsserver attached, stop it if there is a denols server attached
-          elseif curr_client.name == "tsserver" then
-            for _, client in ipairs(vim.lsp.get_active_clients { bufnr = bufnr }) do
-              if client.name == "denols" then
-                vim.lsp.stop_client(curr_client.id, true)
-                break
-              end
-            end
-          end
-        end,
-      })
-      opts.server = require("astronvim.utils.lsp").config "denols"
-      opts.server.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
-    end,
+    init = function() astronvim.lsp.skip_setup = utils.list_insert_unique(astronvim.lsp.skip_setup, "denols") end,
+    ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+    opts = function() return { server = require("astronvim.utils.lsp").config "denols" } end,
   },
 }
