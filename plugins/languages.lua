2139c7932efeea8a49920377926876d3cdb28f55
ForAeons <w.xy020203@gmail.com>
2023-10-05 09:39:48 +0800

    refactor

--- plugins/languages.lua
+++ plugins/languages.lua
@@ -1,3 +1,18 @@
 return {
-  import = "user.plugins.languages"
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript-deno" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.zig" },
 }
