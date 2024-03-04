b62d4c3f7cdd012346be53c75b1e628f7702e2e8
ForAeons <w.xy020203@gmail.com>
2024-03-04 10:29:32 +0800

    Remove neovide config

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
-vim.o.guifont = 'FiraCode Nerd Font Mono:h16'
-vim.g.transparency = 0.9
-vim.g.neovide_window_blurred = true
-vim.g.neovide_refresh_rate = 120
-vim.g.neovide_no_idle = true
-vim.g.neovide_cursor_vfx_mode = "pixiedust"
-
 return {
-  colorscheme = 'tokyonight',
+  colorscheme = 'tokyonight-night',
 }
