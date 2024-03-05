c4fd19e758707c498530d9158e10e84674bad48c
ForAeons <w.xy020203@gmail.com>
2024-03-05 21:19:37 +0800

    Add docker and toml pack

--- plugins/community/language.lua
+++ plugins/community/language.lua
@@ -1,6 +1,7 @@
 return {
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.java" },
@@ -10,5 +11,6 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
 }
