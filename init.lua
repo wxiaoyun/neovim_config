4f6613c33cb60ab11e4f284b1d5e99615cd5430e
ForAeons <w.xy020203@gmail.com>
2024-03-12 16:11:27 +0800

    Update config

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 vim.opt.list = true
-vim.opt.listchars = { eol = "󱞥" }
+vim.opt.listchars = { tab = "\\ ", eol = "󱞥" }
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
+
+vim.opt.scrolloff = 8
 
 return {
   colorscheme = "tokyonight-night",
