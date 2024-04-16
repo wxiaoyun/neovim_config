94bee0a21004b5ba4208bd8cdb696f52bcd9f8a1
ForAeons <w.xy020203@gmail.com>
2024-04-16 10:36:59 +0800

    Update config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -29,14 +29,14 @@ return {
       "yaml",
       -- add more arguments for adding more treesitter parsers
     })
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = "<C-space>",
+        node_decremental = "<BS>",
+      },
+    }
   end,
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<C-space>",
-      node_incremental = "<C-space>",
-      scope_incremental = false,
-      node_decremental = "<BS>",
-    },
-  },
 }
